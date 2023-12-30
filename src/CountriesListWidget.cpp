ad478531b1ee7207d5803d14c3c9664d46ac358f
georgiykulak <kulakgeorgij@gmail.com>
2023-12-30 03:24:29 +0200

    View: Create layout with tree view and scroll bar

--- src/CountriesListWidget.cpp
+++ src/CountriesListWidget.cpp
@@ -1,11 +1,27 @@
 #include "CountriesListWidget.hpp"
 #include "ui_CountriesListWidget.h"
 
+#include <QHBoxLayout>
+#include <QTreeView>
+#include <QTreeWidget>
+#include <QScrollBar>
+
 CountriesListWidget::CountriesListWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::CountriesListWidget)
 {
     ui->setupUi(this);
+
+    QHBoxLayout* layout = new QHBoxLayout;
+    this->setLayout(layout);
+
+    m_treeView = new QTreeView(this);
+    m_treeView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    auto* scrollBar = new QScrollBar(Qt::Orientation::Vertical, this);
+
+    layout->addWidget(m_treeView);
+    layout->addWidget(scrollBar);
 }
 
 CountriesListWidget::~CountriesListWidget()
--- src/CountriesListWidget.hpp
+++ src/CountriesListWidget.hpp
@@ -9,6 +9,8 @@ class CountriesListWidget;
 }
 QT_END_NAMESPACE
 
+class QTreeView;
+
 class CountriesListWidget : public QWidget
 {
     Q_OBJECT
@@ -19,5 +21,6 @@ public:
 
 private:
     Ui::CountriesListWidget *ui;
+    QTreeView* m_treeView;
 };
 #endif // COUNTRIESLISTWIDGET_HPP
